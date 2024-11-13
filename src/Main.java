5ebf3a01bf85d49e8cffc6ff457c177cc62ddece
Basso-Giovanni <9698115@itisrossi.vi.it>
2024-11-13 08:47:00 +0100

    Aggiunta metodo Main e Crawl

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,52 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        //jsop
+        Scanner scanner = new Scanner(System.in);
+        do
+        {
+            System.out.print("Inserisci il seed URL: ");
+            String seed = scanner.next();
+
+            if (seed.startsWith("http"))
+            {
+                crawl(seed, 0);
+                return;
+            }
+            else
+                System.out.println("⚠️ Errore: inserire un link assoluto!");
+        } while (true);
+    }
+
+    public static void crawl(String seed, int depth)
+    {
+        if (depth > 3) return; //caso base
+
+        try
+        {
+            Document doc = Jsoup.connect(seed).get();
+            Elements links = doc.select("a[href]");
+            for (Element link : links)
+            {
+                String newURL = link.absUrl("href");
+                if (newURL.startsWith("http"))
+                {
+                    System.out.println("\tLink: " + newURL);
+                    crawl(newURL, depth + 1);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("⚠️ IOException: errore nel caricamento (" + seed + ")");
+        }
     }
 }
